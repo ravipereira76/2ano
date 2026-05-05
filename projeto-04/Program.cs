cb6c12055aa44bb3bfad0425308bc60c3c2c6be7
ravipereira76 <ravipereira76@gmail.com>
2026-05-05 10:31:31 -0300

    Create Program.cs

---
+++ projeto-04/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine("Funcionario: "+ func);
+
+            Console.WriteLine("");
+            Console.WriteLine("Deseja aumentar o salario em qual porcentagem: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados: "+func);
+
+        }
+    }
+}
