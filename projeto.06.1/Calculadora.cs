50284248970150f66cdbfd2e39e6708c7e489bc0
ravipereira76 <ravipereira76@gmail.com>
2026-05-11 11:49:46 -0300

    Update Calculadora.cs

--- projeto.06.1/Calculadora.cs
+++ projeto.06.1/Calculadora.cs
@@ -8,12 +8,12 @@ namespace Projeto_06._1
 {
     internal class Calculadora
     {
-        public double Pi = 3.14;
-        public double Circunferencia (double r)
+        public  static double Pi = 3.14;
+        public static double Circunferencia (double r)
         {
             return 2.0 * Pi * r;
         }
-        public double Volume (double r)
+        public static double Volume (double r)
         {
             return 4.0 / 3.0 * Pi * r * r * r;
         }
