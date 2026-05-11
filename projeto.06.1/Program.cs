b9b841c88a49663f84140f7b95b6159cf5afe634
ravipereira76 <ravipereira76@gmail.com>
2026-05-11 11:50:12 -0300

    Update Program.cs

--- projeto.06.1/Program.cs
+++ projeto.06.1/Program.cs
@@ -7,6 +7,8 @@ namespace Projeto_06._1
     {
         static void Main(string[] args)
         {
+            /*
+            //Versão 02
             Calculadora calc = new Calculadora();
 
             Console.Write("Entre com o valor do Raio: ");
@@ -17,6 +19,18 @@ namespace Projeto_06._1
             Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            */
+            //Versão 03
+
+            Console.Write("Entre com o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
-}
